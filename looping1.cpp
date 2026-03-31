46a5f538ca79eb30b5f3673f1d5e9104f12808fd
almaudud <almadudiakunngoding@gmail.com>
2026-03-31 22:17:56 +0700

    menambahkan perulangan FOR memasukkan teknologi informasi UMY

--- looping1.cpp
+++ looping1.cpp
@@ -3,3 +3,11 @@
 #include <string>
 using namespace std;
 
+int main() {
+    int x;
+
+    cout << "PERULANGAN FOR" << endl;
+    for(int i = 0; i <= 4; i++) {
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+   
